8cf9c7b2f28040ab6f0189030ca075275296b3b3
Karina Osoprilko <71908284+chamomilek@users.noreply.github.com>
2021-07-13 09:35:42 +0300

    feat: implement Combinator

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,22 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        if (array.isEmpty() || array.size < 2 || array[0] < 0 || array[1] <= 0) return null
+        val m = array[0]
+        val n = array[1]
+        val factorialN = factorial(n)
+        for (i in 1..n) {
+            val nMinusI = n - i
+            val factorialNminusI = factorial(nMinusI)
+            val factorialI = factorial(i)
+            val result = factorialN / factorialNminusI / factorialI
+            if (result >= m) return i
+        }
+        return null
+    }
+
+    private tailrec fun factorial(n: Int, f: Long = 1): Long {
+        return if (n < 2) f else factorial(n - 1, f * n)
     }
 }
